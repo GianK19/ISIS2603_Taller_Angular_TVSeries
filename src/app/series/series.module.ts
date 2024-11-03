26e2d496c9f9e628424bcc1b4d34e73a3c70a778
Gian Lanziano <g.lanziano@uniandes.edu.co>
2024-11-02 19:45:05 -0500

    fix module series

--- src/app/series/series.module.ts
+++ src/app/series/series.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SeriesComponent } from './series.component';
 import { ListSeriesComponent } from './list-series/list-series.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [SeriesComponent, ListSeriesComponent],
+  declarations: [ListSeriesComponent],
   exports: [ListSeriesComponent]
 })
 export class SeriesModule { }
