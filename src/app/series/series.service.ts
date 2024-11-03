0282bd61e15401079e53c2a6801b9cd7e338e58c
Gian Lanziano <g.lanziano@uniandes.edu.co>
2024-11-02 19:43:41 -0500

    create service series

---
+++ src/app/series/series.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Serie } from './serie';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeriesService {
+  private apiUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getSeries(): Observable<Serie[]> {
+    return this.http.get<Serie[]>(this.apiUrl);
+  }
+}
